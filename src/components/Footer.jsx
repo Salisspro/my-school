2634660ccad34dd5fd85832c4a2c710274e0c287
salisspro <salisspro@gmail.com>
2025-04-22 04:13:50 +0100

    new change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,12 @@
 
 export default function Footer() {
   return (
-    <div className="font-mono bg-slate-200 p-5 text-center">
-      <p>Designed and developed by <span className="underline text-blue-700">Salisu Yushau Sulaiman</span></p>
+    <div className="flex items-center justify-between p-2 flex-col">
+      <h1 className='text-slate-500 text-sm p-3 capitalize font-bold'> <span className='text-7xl text-teal-700 font-serif'>s</span >alisspro tech</h1>
+      <p className='text-slate-500 text-sm p-3 capitalize font-bold'>Developed by salisspro</p>
+      <p className='text-slate-500 text-sm p-3 capitalize font-bold'>Maintained by salisspro</p>
+      <p className='text-slate-500 text-sm p-3 capitalize font-bold'>All rights reserved &copy; 2023</p>
+    
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,37 +4,46 @@ import IMG3 from '../assets/images/image-gaming-growth.jpg'
 
 function Hero() {
    return (
-      <div className='m-5 md:grid lg:grid-cols-3 md:space-x-5 '>
-<div className='md:h-[22em]  h-56 mt-10 bg-slate-200 p-5 flex items-center text-slate-600 '>
-            
-            <img className='md:w-full h-36 rounded  w-35 m-2 float-right ' src={IMG1} alt="" />
-            <div>
-            <h1 className='text-sm underline mb-2 text-blue-700 cursor-pointer uppercase'>retro pcs</h1>
-            <p className='capitalize '>Welcome to 3.5webG this sectoin will talk about how to master computer </p>
-            </div>
-            
+      <div className='flex flex-col md:flex-row items-center justify-between p-5 bg-slate-100'>
+         <div className='flex flex-col space-y-5'>
+            <h1 className='text-4xl font-bold text-slate-800'>The Bright Future of Web 3.0?</h1>
+            <p className='text-slate-500'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+            <button className='bg-teal-700 text-slate-100 p-2 rounded-md'>Read More</button>
          </div>
+         <div className='flex flex-col md:flex-row space-x-5 mt-5 md:mt-0'>
+            <div className='flex flex-col space-y-5'>
+               <h1 className='text-2xl font-bold text-slate-800'>New</h1>
+               <img src={IMG1} alt="" className='w-full md:w-[300px] h-[200px] object-cover' />
+               <p className='text-slate-500'>Reviving Retro PCs: How the Next Generation of PC Gaming</p>
+               
 
-         <div className='md:h-[22em]  h-56 mt-10 bg-slate-200 p-5 flex items-center text-slate-600 '>
-            
-            <img className='md:w-full h-36 rounded  w-35 m-2 float-right ' src={IMG3} alt="" />
-            <div>
-            <h1 className='text-sm underline mb-2 text-blue-700 cursor-pointer uppercase'>gaming growth</h1>
-            <p className='capitalize '>Welcome to 3.5webG this sectoin will talk about how to master computer </p>
             </div>
-            
-         </div>
 
-         <div className='md:h-[22em]  h-56 mt-10 bg-slate-200 p-5 flex items-center text-slate-600 '>
-            
-            <img className='md:w-full h-36 rounded  w-35 m-2 float-right ' src={IMG2} alt="" />
-            <div>
-            <h1 className='text-sm underline mb-2 text-blue-700 cursor-pointer uppercase'>web 3.0</h1>
-            <p className='capitalize '>Welcome to 3.5webG this sectoin will talk about how to master computer </p>
+
+            <div className='flex flex-col space-y-5 mt-10'>
+               <h1 className='text-2xl font-bold text-slate-800'>New</h1>
+               <img src={IMG2} alt="" className='w-full md:w-[300px] h-[200px] object-cover' />
+               <p className='text-slate-500'>The Downsides of AI Artistry: Can Creatives Face AI-Generated Art?</p>
+               <p className='text-slate-500'>Is AI gonna take over the world?</p>
+               <button className='bg-teal-700 text-slate-100 p-2 rounded-md'>Read More</button>
+
+
+
             </div>
-            
+
+
+            <div className='flex flex-col space-y-5 mt-21'>
+
+               <h1 className='text-2xl font-bold text-slate-800'>New</h1>
+
+               <img src={IMG3} alt="" className='w-full md:w-[300px] h-[200px] object-cover' />
+               <p className='text-slate-500'>The Growth of Gaming: How Gamers are Changing the World</p>
+               <p className='text-slate-500'>How gamers are changing the world?</p>
+               <button className='bg-teal-700 text-slate-100 p-2 rounded-md'>Read More</button>
+            </div>
          </div>
       </div>
+
    )
 }
 
